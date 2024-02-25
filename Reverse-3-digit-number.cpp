e8c8d2fb0ab032e6da300caeb914c9f59ad8c7af
Amiran Tabidze <81627817+amirantabidze2021@users.noreply.github.com>
2024-02-25 20:49:02 +0400

    Update Reverse-3-digit-number.cpp

--- Reverse-3-digit-number.cpp
+++ Reverse-3-digit-number.cpp
@@ -5,7 +5,7 @@ int main()
     int n,n2,n3,n4;
     cout<<"Enter a number: ";
     cin>>n;
-    if(99<n&&n<1000&&n%100!=0 ){
+    if(99<n&&n<1000&&n%100!=0 ){ 
         n2=n%10;
         n3=(n%100-n2)/10;
         n=n/100;
